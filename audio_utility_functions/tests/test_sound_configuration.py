9c6c06de1c5954f1aaed551cfcb3934cb2bf8eb4
Danial Nayyar (MEng, Hons) <110974901+DanialNayyar@users.noreply.github.com>
2026-06-06 17:29:19 +0100

    Add files via upload

---
+++ audio_utility_functions/tests/test_sound_configuration.py
@@ -0,0 +1,125 @@
+from sound_configuration import configuration
+import pytest
+
+def test_sound_config_is_created():
+    #arrange
+
+    frequency = 400
+    duration = 1.0
+    waveform = "sine"
+    volume = 0.5
+
+    #act
+
+    config = configuration(frequency=frequency,
+                            duration = duration, 
+                           waveform=waveform,
+                           volume= volume) 
+    
+
+    assert config["frequency"] == frequency
+    assert config["duration"] == duration
+    assert config["waveform"] == waveform
+    assert config["volume"] == volume
+
+
+
+def test_negative_frequency_error():
+
+    with pytest.raises(ValueError):
+
+        configuration(frequency=-400,
+                      duration = 1.0,
+                      waveform = "sine",
+                      volume = 0.5)
+
+
+
+def test_zero_frequency_error():
+
+    with pytest.raises(ValueError):
+
+        configuration(frequency=0,
+                      duration = 1.0,
+                      waveform = "sine",
+                      volume = 0.5)
+
+
+
+
+
+
+def test_zero_duration_error():
+    
+    with pytest.raises(ValueError):
+
+        configuration(frequency=400,
+                      duration = 0.0,
+                      waveform = "sine",
+                      volume = 0.5)
+
+
+
+
+def test_negative_duration_error():
+
+    with pytest.raises(ValueError):
+
+        configuration(frequency=400,
+                      duration = -1.0,
+                      waveform = "sine",
+                      volume = 0.5)
+        
+
+
+
+
+
+
+def test_invalid_waveform_selected():
+
+    with pytest.raises(ValueError):
+
+        configuration(frequency=400,
+                      duration = 1.0,
+                      waveform = "blue",
+                      volume = 0.5)
+        
+
+
+
+
+
+def test_zero_volume_error():
+
+    with pytest.raises(ValueError):
+
+        configuration(frequency=400,
+                      duration = 1.0,
+                      waveform = "sine",
+                      volume = 0.0)
+        
+
+
+
+def test_negative_volume_error():
+
+    with pytest.raises(ValueError):
+
+        configuration(frequency=400,
+                      duration = 1.0,
+                      waveform = "sine",
+                      volume = -0.5)
+
+
+
+
+
+def test_volume_above_one_error():
+
+    with pytest.raises(ValueError):
+
+        configuration(frequency=400,
+                      duration = 1.0,
+                      waveform = "sine",
+                      volume = 1.5)
